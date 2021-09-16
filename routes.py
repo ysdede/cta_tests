fbf25cf35ec19b2bb758e03d257704298291f388
ysdede <yunusseyhandede@gmail.com>
2021-09-16 21:57:53 +0300

    config and routes examples

---
+++ routes.py
@@ -0,0 +1,14 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Make sure to read the docs about routes if you haven't already:
+# https://docs.jesse.trade/docs/routes.html
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# trading routes
+routes = [
+    ('Binance Futures', 'ETH-USDT', '2h', 'BBTest'),
+    # ('Binance Futures', 'ETH-USDT', '2h', 'BBRTest'),
+]
+
+# in case your strategy requires extra candles, timeframes, ...
+extra_candles = [
+]
